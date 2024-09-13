2e0af819ea1d1161fe208c215170d788da5a8bcf
José Vitor Nunes de Morais <josevitornm2014@outlook.com>
2024-09-13 11:33:57 -0300

    começando com Postman

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 // Instalar a dependencia do Express
 const app = express();
 
 // Criação de rota
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     res.send("Bem vindo ao Curso de Node.js - tsc-watch")
+})
+
+app.get("/users", (req: Request, res: Response) => {
+    
+    let usuarios = [
+        {
+            nome: "José vitor",
+            idade: 26
+        }, {
+            nome: "Derci Santos",
+            idade: 33
+        }
+    ]
+    
+    // mostrando o array no postman
+    res.send(usuarios)
 })
 
 // Criação do servidor com Express
