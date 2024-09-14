571e8123ec8eb06067b524452a6ee614027525b6
José Vitor - Casa <josevitornm2014@outlook.com>
2024-09-14 15:15:34 -0300

    Metodos HTTP

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
 import express, { Request, Response } from "express";
 
 // Instalar a dependencia do Express
-const app = express();
+const app = express()
+
+app.use(express.json())
 
 // Criação de rota
 app.get("/", (req: Request, res: Response) => {
     res.send("Bem vindo ao Curso de Node.js - tsc-watch")
 })
 
+let usuarios = [
+    {
+        nome: "José vitor",
+        idade: 26
+    }, {
+        nome: "Derci Santos",
+        idade: 33
+    }
+]
+
 app.get("/users", (req: Request, res: Response) => {
-    
-    let usuarios = [
-        {
-            nome: "José vitor",
-            idade: 26
-        }, {
-            nome: "Derci Santos",
-            idade: 33
-        }
-    ]
-    
     // mostrando o array no postman
     res.send(usuarios)
+})
+
+app.post("/users", (req: Request, res: Response) => {
+    let user = req.body
+    usuarios.push(user)
+    res.send({
+        message: "Usuário criado com sucesso!"
+    })
 })
 
 // Criação do servidor com Express
