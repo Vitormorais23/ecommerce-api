c7b66e4be20d8c3c65f43bbdb9711bbbe23e1d80
José Vitor - Casa <josevitornm2014@outlook.com>
2024-09-14 15:38:35 -0300

    Parâmetros de Rota | GET

--- src/index.ts
+++ src/index.ts
@@ -10,23 +10,25 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Bem vindo ao Curso de Node.js - tsc-watch")
 })
 
-let usuarios = [
-    {
-        nome: "José vitor",
-        idade: 26
-    }, {
-        nome: "Derci Santos",
-        idade: 33
-    }
-]
+let id = 1
+let usuarios: {id: number, nome: string, email: string} [] = []
 
 app.get("/users", (req: Request, res: Response) => {
     // mostrando o array no postman
     res.send(usuarios)
 })
 
+// comparar para encontrar id do usuario
+app.get("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id)
+    let user = usuarios.find(user => user.id === userId)
+    res.send(user)
+})
+
+// cadastrar usuario
 app.post("/users", (req: Request, res: Response) => {
     let user = req.body
+    user.id = id++
     usuarios.push(user)
     res.send({
         message: "Usuário criado com sucesso!"
