c74df400291e702b46bc27d616952cd705644427
José Vitor Nunes de Morais <josevitornm2014@outlook.com>
2024-09-17 17:10:24 -0300

    Usando os Controllers

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+
+type User = {
+    id: number;
+    nome: string;
+    email: string;
+}
+let id = 1
+let usuarios: User[] = []
+
+export class UsersController {
+    // retorna lista de todos usuários
+    static gatAll(req: Request, res: Response) {
+        res.send(usuarios)
+    }
+
+    // comparar para encontrar id do usuario
+    static getById(req: Request, res: Response) {
+        let userId = Number(req.params.id)
+        let user = usuarios.find(user => user.id === userId)
+        res.send(user)
+    }
+
+    // cadastrar usuario
+    static save(req: Request, res: Response) {
+        let user = req.body
+        user.id = id++
+        usuarios.push(user)
+        res.send({
+            message: "Usuário criado com sucesso!"
+        })
+    }
+
+    // Atualizar usuario
+    static updateUser(req: Request, res: Response) {
+        let userId = Number(req.params.id)
+        let user = req.body
+        let indexOf = usuarios.findIndex((_user: User) => _user.id === userId)
+        usuarios[indexOf].nome = user.nome
+        usuarios[indexOf].email = user.email
+        res.send({
+            message: "Usuário atualizado com sucesso!"
+        })
+    }
+
+    // Deletar usuário
+    static delete(req: Request, res: Response) {
+        let userId = Number(req.params.id);
+        let indexOf = usuarios.findIndex((user: User) => user.id === userId);
+        if (indexOf !== -1) /* A verificação indexOf !== -1 é importante para garantir que o usuário com o ID fornecido exista no array antes de tentar removê-lo */ {
+            usuarios.splice(indexOf, 1); // Remove o usuário do array
+            res.send({
+                message: "Usuario excluído com sucesso!"
+            })
+        } else {
+            res.status(404).send({
+                message: "Usuário não encontrado"
+            });
+        }
+    }
+}
