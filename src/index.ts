864b01c3e4fa502ccd9ffec793a359b0a1cf4a80
José Vitor Nunes de Morais <josevitornm2014@outlook.com>
2024-09-17 14:51:29 -0300

    Parâmetros de rota | PUT e DELETE

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,14 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Bem vindo ao Curso de Node.js - tsc-watch")
 })
 
+type User = {
+    id: number;
+    nome: string;
+    email: string;
+}
+
 let id = 1
-let usuarios: {id: number, nome: string, email: string} [] = []
+let usuarios: User[] = []
 
 app.get("/users", (req: Request, res: Response) => {
     // mostrando o array no postman
@@ -34,6 +40,33 @@ app.post("/users", (req: Request, res: Response) => {
         message: "Usuário criado com sucesso!"
     })
 })
+
+// Atualizar usuario
+app.put("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id)
+    let user = req.body
+    let indexOf = usuarios.findIndex((_user: User) => _user.id === userId)
+    usuarios[indexOf].nome = user.nome
+    usuarios[indexOf].email = user.email
+    res.send({
+        message: "Usuário atualizado com sucesso!"
+    })
+})
+
+app.delete("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let indexOf = usuarios.findIndex((user: User) => user.id === userId);
+    if (indexOf !== -1) /* A verificação indexOf !== -1 é importante para garantir que o usuário com o ID fornecido exista no array antes de tentar removê-lo */{
+        usuarios.splice(indexOf, 1); // Remove o usuário do array
+        res.send({
+            message: "Usuario excluído com sucesso!"
+        })
+    } else {
+        res.status(404).send({
+            message: "Usuário não encontrado"
+        });
+    }
+});
 
 // Criação do servidor com Express
 app.listen(3000, () => {
